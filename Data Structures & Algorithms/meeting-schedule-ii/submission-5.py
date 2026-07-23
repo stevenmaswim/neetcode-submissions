c89316f5ce55c808943b46855f451294236932cd
stevenmaswim <stevenmaswim@gmail.com>
2026-07-23 15:43:04 -0500

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        max_rooms = 0
+        rooms = 0 
+        starts = sorted(i.start for i in intervals)
+        ends = sorted(i.end for i in intervals)
+        i = 0
+        j = 0 
+        while i < len(intervals):
+            if starts[i] < ends[j]:
+                i += 1
+                rooms += 1 
+            else: 
+                j += 1
+                rooms -= 1
+
+            max_rooms = max(max_rooms, rooms)
+        return max_rooms
